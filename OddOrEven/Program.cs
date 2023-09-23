ee0455f800c141ee7a022bb953716ab78971e563
Kert Järva <kert.jarva@tktk.ee>
2023-09-23 18:24:16 +0300

    add week3 code

---
+++ OddOrEven/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.Design;
+
+Console.WriteLine("Enter a number");
+int userNum = Convert.ToInt32(Console.ReadLine());  
+// kontrollime kas kasutaja arv on paaris või paaritu
+
+int result = userNum % 2;
+
+
+if (result != 0) 
+{
+    Console.WriteLine("User number is odd.");
+}
+else
+{
+    Console.WriteLine("User number is even.");
+}
---
+++ Week3IfAndSwitch/Program.cs
@@ -0,0 +1,5 @@
+﻿int a = 24;
+int b = 8;
+
+int c = b % a;
+Console.WriteLine(c);
